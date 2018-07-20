9d2634478839e18894091a27a4e377054099e638
Ryan Heise <heis007ryan@gmail.com>
2018-07-20 18:26:38 -0500

    MVP complete and some css

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 $(document).ready(function() {
   //attach event listner to button click(input)
-  $('.store-btn').click(function() {
-    localStorage.setItem("hrext", "three is best");
+//localStorage.length as your array length
+// localStorage.key(index) to access by number
+// that returns a list of the keys
+
+  $('.store-btn').click(function(event) {
+    let titleValue = $('.input-field-title').val();
+    let contentValue = $('.input-field-body').val();
+    localStorage.setItem('titleValue', titleValue);
+    localStorage.setItem('contentValue', contentValue);
+
   });
 
-  $('.get-btn').click(function() {
-   console.log(localStorage.getItem('hrext'))
+  $('.get-btn').click(function(event) {
+  let titleValue = localStorage.getItem('titleValue')
+  let contentValue = localStorage.getItem('contentValue')
+   $('.debug').html(`<p>${titleValue} ${contentValue}</p>`)
   });
 
   $('.delete-btn').click(function() {
-   localStorage.removeItem('hrext');
+    localStorage.removeItem('titleValue')
+    localStorage.removeItem('contentValue')
+    $('.debug').html(`<p>have been deleted</p>`)
+    // TODO: confirm wanting to delete (window confirm method??)
+    //throw up .confirm window
+    //
   });
 
 //CLEAR ALL local storage BUTTON
