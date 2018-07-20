4dc99cf9a6e7f37f78f277dd6a6dafba74413438
Ryan Heise <heis007ryan@gmail.com>
2018-07-20 13:03:09 -0500

    working on click events

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  //attach event listner to button click(input)
+  $('.').click(function() {
+
+  });
+
+  $('.').click(function() {
+
+  });
+
+  $('.').click(function() {
+
+  });
+  //create function skelton stub for read/write/delete
+  //research local storage
+});
