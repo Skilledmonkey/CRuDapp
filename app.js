3bf451e371a073a7bc503717d4744ec72a14ea98
Ryan Heise <heis007ryan@gmail.com>
2018-07-20 13:29:12 -0500

    click events added

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
   //attach event listner to button click(input)
-  $('.').click(function() {
-
+  $('.store-btn').click(function() {
+    localStorage.setItem("hrext", "three is best");
   });
 
-  $('.').click(function() {
-
+  $('.get-btn').click(function() {
+   console.log(localStorage.getItem('hrext'))
   });
 
-  $('.').click(function() {
-
+  $('.delete-btn').click(function() {
+   localStorage.removeItem('hrext');
   });
+
+//CLEAR ALL BUTTON
+  $('.').click(function() {
+   localStorage.clear();
+  });
+
+
   //create function skelton stub for read/write/delete
   //research local storage
 });
